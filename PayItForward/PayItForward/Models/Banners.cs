185946dba6bcebceae21f71233e57e4b0dd0a0f4
MARIANO LEON RESIO ELCARAS <43720166@comunidadort.edu.ar>
2019-08-23 10:46:12 -0300

    usuarios - model

--- PayItForward/PayItForward/Models/Banners.cs
+++ PayItForward/PayItForward/Models/Banners.cs
@@ -8,12 +8,10 @@ namespace PayItForward.Models
     public class Banners
     {
         private int idBanner;
-        private DateTime desde;
         private DateTime hasta;
         private string imagen;
 
         public int IdBanner { get => idBanner; set => idBanner = value; }
-        public DateTime Desde { get => desde; set => desde = value; }
         public DateTime Hasta { get => hasta; set => hasta = value; }
         public string Imagen { get => imagen; set => imagen = value; }
 
@@ -22,10 +20,9 @@ namespace PayItForward.Models
 
         }
 
-        public Banners(int idBanner, DateTime desde, DateTime hasta, string imagen)
+        public Banners(int idBanner, DateTime hasta, string imagen)
         {
             this.idBanner = idBanner;
-            this.desde = desde;
             this.hasta = hasta;
             this.imagen = imagen;
         }
---
+++ PayItForward/PayItForward/Models/Usuarios.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PayItForward.Models
+{
+    public class Usuarios
+    {
+        private int _IdUsuario;
+        private string _Nombre;
+        private string _Apellido;
+        private string _Mail;
+        private string _Contrasena;
+        private string _Imagen;
+        private int _Puntos;
+
+        public Usuarios(int IdUsuario, string Nombre, string Apellido, string Mail, string Contrasena, string Imagen, int Puntos)
+        {
+            _IdUsuario = IdUsuario;
+            _Nombre = Nombre;
+            _Apellido = Apellido;
+            _Mail = Mail;
+            _Contrasena = Contrasena;
+            _Imagen = Imagen;
+            _Puntos = Puntos;
+        }
+
+        public Usuarios() { }
+
+        public int IdUsuario { get => _IdUsuario; set => _IdUsuario = value; }
+        public string Nombre { get => _Nombre; set => _Nombre = value; }
+        public string Apellido { get => _Apellido; set => _Apellido = value; }
+        public string Mail { get => _Mail; set => _Mail = value; }
+        public string Contrasena { get => _Contrasena; set => _Contrasena = value; }
+        public string Imagen { get => _Imagen; set => _Imagen = value; }
+        public int Puntos { get => _Puntos; set => _Puntos = value; }
+    }
+}
